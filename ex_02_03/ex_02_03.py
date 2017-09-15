ad3eab2bfc9c856988e98c0056fc9d941facc24a
DAVID <davidnmwood@gmail.com>
2017-09-15 13:03:21 -0400

    Overtime hours exercise completed

--- ex_02_03/ex_02_03.py
+++ ex_02_03/ex_02_03.py
@@ -1,4 +1,10 @@
-xh = input("Enter hours: ")
-xh = int(xh)
+hrs = input("Enter Hours:")
+h = float(hrs)
+rte = input("Enter rate:")
+r = float(rte)
 
-print(xh)
+oth = h - 40
+if h <= 40:
+	print(h * r)
+elif h > 40:
+    print((40 * r) + (oth * (r * 1.5)))
