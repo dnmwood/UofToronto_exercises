679146e84bfec7562be30c8fbc4cafa1f3b5a04e
DAVID <davidnmwood@gmail.com>
2017-09-15 13:06:53 -0400

    Tidy up this code

--- ex_02_03/ex_02_03.py
+++ ex_02_03/ex_02_03.py
@@ -3,8 +3,13 @@ h = float(hrs)
 rte = input("Enter rate:")
 r = float(rte)
 
-oth = h - 40
+#overtime
+othrs = (h - 40)
+otrte = (r * 1.5)
+
+#regular hours
 if h <= 40:
 	print(h * r)
+#overtime hours
 elif h > 40:
-    print((40 * r) + (oth * (r * 1.5)))
+    print((40 * r) + (othrs * otrte))
