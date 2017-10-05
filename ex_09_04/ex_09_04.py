c1ca8823a0aed2649872146afb475152bcc59aeb
DAVID <davidnmwood@gmail.com>
2017-10-04 23:39:11 -0400

    Complete exercise 9.4

--- ex_09_04/ex_09_04.py
+++ ex_09_04/ex_09_04.py
@@ -1,3 +1,18 @@
-name = input("Enter file:")
+emails = dict()
+most_emails = 0
+
+name = input("Enter file: ")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
+
+for line in handle:
+    if not line.startswith("From ") : continue
+    words = line.split()
+    e = words[1]
+    emails[e] = emails.get(e,0) + 1
+    if emails[e] > most_emails:
+        most_emails += 1
+
+for e in emails:
+    if emails[e] == most_emails:
+        print(e, emails[e])
