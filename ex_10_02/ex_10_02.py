893eecd7cce62e2fba7ec6d0fc51e24a7620a340
DAVID <davidnmwood@gmail.com>
2017-10-06 19:44:17 -0400

    Complete exercise 10.2

--- ex_10_02/ex_10_02.py
+++ ex_10_02/ex_10_02.py
@@ -1,3 +1,20 @@
 name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
+
+hrs = dict()
+for line in handle:
+    if not line.startswith("From ") : continue
+    words = line.split()
+    tme = words[5]
+    hr = tme[:2]
+    hrs[hr] = hrs.get(hr,0) + 1
+
+lst = list()
+for key, val in hrs.items():
+    newtup = (key, val)
+    lst.append(newtup)
+
+lst = sorted(lst)
+for key, val in lst:
+    print(key, val)
